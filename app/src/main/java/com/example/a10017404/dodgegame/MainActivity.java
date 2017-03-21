63bd5728076efdf7f62d3f7ced6e0fb60dd104f4
Vineeth Vattikuti <vinkriv@gmail.com>
2017-03-21 09:54:58 -0500

    v

---
+++ app/src/main/java/com/example/a10017404/dodgegame/MainActivity.java
@@ -0,0 +1,108 @@
+package com.example.a10017404.dodgegame;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Point;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Display;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+public class MainActivity extends AppCompatActivity {
+    //Code from this program has been used from "Beginning Android Games" by Mario Zechner
+    //Review SurfaceView, Canvas, continue
+
+    GameSurface gameSurface;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        gameSurface = new GameSurface(this);
+        setContentView(gameSurface);
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        gameSurface.pause();
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        gameSurface.resume();
+    }
+
+
+
+    //----------------------------GameSurface Below This Line--------------------------
+    public class GameSurface extends SurfaceView implements Runnable {
+
+        Thread gameThread;
+        SurfaceHolder holder;
+        volatile boolean running = false;
+        Bitmap myImage;
+        Paint paintProperty;
+
+        int screenWidth;
+        int screenHeight;
+
+        public GameSurface(Context context) {
+            super(context);
+
+            holder=getHolder();
+
+            myImage = BitmapFactory.decodeResource(getResources(),R.drawable.spaceship);
+
+            Display screenDisplay = getWindowManager().getDefaultDisplay();
+            Point sizeOfScreen = new Point();
+            screenDisplay.getSize(sizeOfScreen);
+            screenWidth=sizeOfScreen.x;
+            screenHeight=sizeOfScreen.y;
+
+            paintProperty= new Paint();
+            paintProperty.setTextSize(100);
+
+        }
+
+        @Override
+        public void run() {
+            while (running){
+
+                if (holder.getSurface().isValid() == false)
+                    continue;
+
+                Canvas canvas= holder.lockCanvas();
+                canvas.drawRGB(255,0,0);
+                int value = 5;
+                canvas.drawBitmap(myImage,500+value,1100,null);
+
+                holder.unlockCanvasAndPost(canvas);
+            }
+        }
+
+        public void resume(){
+            running=true;
+            gameThread=new Thread(this);
+            gameThread.start();
+        }
+
+        public void pause() {
+            running = false;
+            while (true) {
+                try {
+                    gameThread.join();
+                } catch (InterruptedException e) {
+                }
+            }
+        }
+
+
+    }//GameSurface
+}//Activity
